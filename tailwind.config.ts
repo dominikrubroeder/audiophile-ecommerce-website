17a0b4fd3397c910e975b1af9a07c640d2c163c1
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-07-09 09:40:58 +0200

    Start Typography setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,26 @@
-import type { Config } from "tailwindcss";
+import type {Config} from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                app: {
+                    'white': 'hsl(21, 94%, 75%)',
+                    'black': 'hsl(21, 94%, 75%)',
+                    'neutral-50': 'hsl(0, 0%, 98%)',
+                    'neutral-100': 'hsl(0, 0%, 95%)',
+                    'neutral-900': 'hsl(0, 0%, 6%)',
+                    'brown': 'hsl(21, 94%, 75%)',
+                    'salmon': 'hsl(21, 94%, 75%)',
+                }
+            }
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
